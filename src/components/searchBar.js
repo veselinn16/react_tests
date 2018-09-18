17219c971aabf96fff94a61af22070c7cdf80ba3
veselinn16 <veselinn16@gmail.com>
2018-09-18 12:15:32 +0300

    Add state to SearchBar component :wrench:

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    state = {
+        query: ''
+    }
+
+    onInputChange = e => { 
+        this.setState({query: e.target.value});
+        console.log(this.state.query)
+    }
+    
     render() {
-        return <input />;
+        return <input onChange={this.onInputChange} />;
     } 
 }
 
