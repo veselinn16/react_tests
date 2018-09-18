9dbc4f87416db2cb014b712fe65debb2c6849516
veselinn16 <veselinn16@gmail.com>
2018-09-18 13:06:28 +0300

    Get data from YouTube and put it in state :wrench:

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchBar';
+import YTSearch from 'youtube-api-search';
 
 export const apiKey = 'AIzaSyB0WZbsy37TjcUDNYSq2eMyFgLMfjy-oL0';
 
+
+
 // Create component, producing HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: []};
+
+        YTSearch({key: apiKey, term: 'surfing'}, videos => {
+            this.setState({ videos });
+        })    
+    }
+
+    render () {
+        return (
+            <div>
+              <SearchBar/>
+            </div>
+        );
+    }
 }
 
 
